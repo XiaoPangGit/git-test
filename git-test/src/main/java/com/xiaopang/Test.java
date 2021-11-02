ddfb676ca48748f45f2ac5ec11ca7d3ffb46f7c7
xiaopang <xiaopang@163.com>
2021-11-02 21:49:11 +0800

    commit2

--- git-test/src/main/java/com/xiaopang/Test.java
+++ git-test/src/main/java/com/xiaopang/Test.java
@@ -6,5 +6,6 @@ package com.xiaopang;
 public class Test {
     public static void main(String[] args) {
         System.out.println("master commit");
+        System.out.println("commit2");
     }
 }
