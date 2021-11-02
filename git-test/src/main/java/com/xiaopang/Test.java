8159d3e4b964062830c6f04a78e59b3a64359c45
xiaopang <xiaopang@163.com>
2021-11-02 21:53:56 +0800

    hot-fix commit1

--- git-test/src/main/java/com/xiaopang/Test.java
+++ git-test/src/main/java/com/xiaopang/Test.java
@@ -7,5 +7,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("master commit");
         System.out.println("commit2");
+        System.out.println("hot-fix commit1");
     }
 }
