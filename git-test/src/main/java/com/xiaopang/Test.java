c89da114b5508fabc03d93499dc5e88abc05404a
xiaopang <xiaopang@163.com>
2021-11-02 21:48:18 +0800

    commit1

---
+++ git-test/src/main/java/com/xiaopang/Test.java
@@ -0,0 +1,10 @@
+package com.xiaopang;
+
+/**
+ * @author xiaopang Gan
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("master commit");
+    }
+}
